f214fb5322c6edd95a94e0ba051194bda4a579b0
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-06 19:34:11 +0100

    Risoluzione Esercizio 02

--- Typescript/Esercizio 02/exercise-2-function-types-start.ts
+++ Typescript/Esercizio 02/exercise-2-function-types-start.ts
@@ -4,29 +4,35 @@
 
 // Add types to this function declaration.
 
-function doubleThePopulation(value) {
-    return value * 2;
+function doubleThePopulation(value): number {
+  return value * 2;
 }
 
 // Correct or remove the function calls which cause type errors.
 
 doubleThePopulation(5);
 
-doubleThePopulation("6.12");
+doubleThePopulation(6.12);
 
 doubleThePopulation(8_526);
 
-doubleThePopulation(false);
+doubleThePopulation(0);
 
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
 
-function languagesSpoken(country: string, language1: string, language2: string): void {
-    console.log(`The languages spoken in ${country} are:`);
+function languagesSpoken(
+  country: string,
+  language1: string,
+  language2?: string
+): void {
+  console.log(`The languages spoken in ${country} are:`);
 
-    console.log(language1);
+  console.log(language1);
 
+  if (typeof language2 === "string") {
     console.log(language2);
+  }
 }
 
 languagesSpoken("Colombia", "Spanish", "English");
