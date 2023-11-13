21ea214bdc758a1a7e6a270a64965e39c6107036
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-13 16:57:44 +0100

    Risoluzione Esercizio 19

--- Typescript/Esercizio 19/exercise-2-create-intersection-types-start.ts
+++ Typescript/Esercizio 19/exercise-2-create-intersection-types-start.ts
@@ -3,16 +3,16 @@
  */
 
 interface Country {
-    name: string;
-    code: string;
+  name: string;
+  code: string;
 }
 
 interface CountryStatistics {
-    population: number;
+  population: number;
 }
 
 interface CountryLanguages {
-    languages: string[];
+  languages: string[];
 }
 
 // Change the `CountryWithLanguages` type into an intersection type
@@ -20,12 +20,12 @@ interface CountryLanguages {
 //
 // This should fix the type error on Line 28.
 
-type CountryWithLanguages = Country;
+type CountryWithLanguages = Country & CountryLanguages;
 
 const countryA: CountryWithLanguages = {
-    name: "Greece",
-    code: "GR",
-    languages: ["Greek"]
+  name: "Greece",
+  code: "GR",
+  languages: ["Greek"],
 };
 
 // Create an intersection type named `CountryWithStatistics` that
@@ -36,11 +36,12 @@ const countryA: CountryWithLanguages = {
 //
 // Add a type annotation with the `CountryWithStatistics` type on
 // the variable `countryB`.
+type CountryWithStatistics = Country & CountryStatistics;
 
-const countryB = {
-    name: "China",
-    code: "CN",
-    population: 1_412_600_000
+const countryB: CountryWithStatistics = {
+  name: "China",
+  code: "CN",
+  population: 1_412_600_000,
 };
 
 // ----
