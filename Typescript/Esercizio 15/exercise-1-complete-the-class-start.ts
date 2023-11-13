63864eabecd31f49b3b29be043bea428d9cc550f
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-13 16:33:34 +0100

    Risoluzione Esercizio 15

--- Typescript/Esercizio 15/exercise-1-complete-the-class-start.ts
+++ Typescript/Esercizio 15/exercise-1-complete-the-class-start.ts
@@ -3,41 +3,41 @@
  */
 
 class Currency {
-    // Add types for these fields and make them private.
-    // Hint: Look at the type of values being passed when `new Currency()`
-    // is called below.
+  // Add types for these fields and make them private.
+  // Hint: Look at the type of values being passed when `new Currency()`
+  // is called below.
 
-    name;
-    code;
-    symbol;
+  private name: string;
+  private code: string;
+  private symbol: string;
 
-    // Add types to the constructor parameters.
+  // Add types to the constructor parameters.
 
-    constructor(name, code, symbol) {
-        this.name = name;
+  constructor(name: string, code: string, symbol: string) {
+    this.name = name;
+    this.code = code;
+    this.symbol = symbol;
 
-        // Store the `code` and `symbol` parameter values in
-        // their corresponding class fields.
+    // Store the `code` and `symbol` parameter values in
+    // their corresponding class fields.
+  }
 
+  // Methods are public by default.
+  // Add a visibility modifier to make it clear this method is public.
+  //
+  // Add a return type for this method
+  // Hint: Look at the inferred return type.
 
-    }
+  public describe(): void {
+    // Replace the `null` values below with the appropriate field values.
+    // Hint: Access field values with: this.fieldName
 
-    // Methods are public by default.
-    // Add a visibility modifier to make it clear this method is public.
-    //
-    // Add a return type for this method
-    // Hint: Look at the inferred return type.
+    let description = `The ${null} currency `;
+    description += `has the code ${null} `;
+    description += `and uses the symbol ${null}.`;
 
-    describe() {
-        // Replace the `null` values below with the appropriate field values.
-        // Hint: Access field values with: this.fieldName
-
-        let description = `The ${null} currency `;
-        description += `has the code ${null} `;
-        description += `and uses the symbol ${null}.`;
-
-        console.log(description);
-    }
+    console.log(description);
+  }
 }
 
 // ----
@@ -47,9 +47,7 @@ const currencyNaira = new Currency("Naira", "NGN", "₦");
 console.log(currencyNaira);
 
 // Call the `describe()` method on the `currencyNaira` object.
-
-
-
+currencyNaira.describe();
 // ----
 
 const currencyUsDollar = new Currency("United States dollar", "USD", "$");
@@ -57,9 +55,7 @@ const currencyUsDollar = new Currency("United States dollar", "USD", "$");
 console.log(currencyUsDollar);
 
 // Call the `describe()` method on the `currencyUsDollar` object.
-
-
-
+currencyUsDollar.describe();
 // ----
 
 export {};
